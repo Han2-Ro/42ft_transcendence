84e998ec12793c2f2a66b1fce03c3a06cf6184c7
Anton <119614085+aprevrha@users.noreply.github.com>
2026-03-17 14:15:34 +0100

    fix: format

--- nextjs/src/app/not-found.tsx
+++ nextjs/src/app/not-found.tsx
@@ -5,7 +5,12 @@ export default function Page() {
     <div className="w-full p-4 flex flex-col items-center gap-10 min-h-screen justify-center">
       <h1 className="text-7xl font-bold text-accent-primary">404</h1>
       <p className="text-2xl">Page not found</p>
-      <Link href="/" className="mt-4 px-6 py-2 bg-accent-primary rounded hover:opacity-80">Go back home</Link>
+      <Link
+        href="/"
+        className="mt-4 px-6 py-2 bg-accent-primary rounded hover:opacity-80"
+      >
+        Go back home
+      </Link>
     </div>
   );
 }
