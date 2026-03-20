76c34cbad641850c1498603772bb49830ccebef8
Han2-Ro <127860003+Han2-Ro@users.noreply.github.com>
2026-03-20 17:51:05 +0100

    feat(frontend): move Footer to layout so it is on every page rm More button as the Links are in the Footer now

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -5,6 +5,7 @@ import NavigationBar from "../componets/NavigationBar";
 import { getSession } from "@/lib/auth/session";
 import AuthProvider from "@/componets/AuthProvider";
 import SidebarActionsProvider from "@/componets/SidebarActionsProvider";
+import Footer from "./Footer";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,13 +32,14 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} h-screen antialiased flex flex-col lg:flex-row`}
+        className={`${geistSans.variable} ${geistMono.variable} min-h-screen antialiased flex flex-col lg:flex-row`}
       >
         <AuthProvider initialUser={user}>
           <SidebarActionsProvider>
             <NavigationBar />
-            <div className="w-full flex-1 bg-background-primary">
-              {children}
+            <div className="w-full min-h-full flex flex-col flex-1 bg-background-primary">
+              <div className="flex-1">{children}</div>
+              <Footer />
             </div>
           </SidebarActionsProvider>
         </AuthProvider>
--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,34 +1,30 @@
 import LeaderBoard from "@/componets/LeaderBoard";
-import Footer from "./Footer";
 import Link from "next/link";
 import GameHistory from "./history/GameHistory";
 
 export default function Home() {
   return (
-    <div className="h-full flex flex-col">
-      <main className="flex-1 p-8 flex flex-col items-center">
-        <h1 className="mb-8 text-6xl">Chess 42</h1>
-        <p className="text-4xl">Better than chess.com!</p>
-        <section className="grid lg:grid-cols-2 py-10 gap-10 w-full">
-          <div className="bg-linear-110 from-gray-600/50 to bg-neutral-800 p-2 rounded-xl">
-            <Link href="/stats">
-              <h2 className="text-2xl w-full text-center pb-4 pt-2 hover:text-stone-400">
-                Leaderboard
-              </h2>
-            </Link>
-            <LeaderBoard maxEntries={3} />
-          </div>
-          <div className="bg-linear-230 from-gray-600/50 to-neutral-800 p-4 rounded-xl">
-            <Link href="/history">
-              <h2 className="text-2xl text-center pb-4 hover:text-stone-400">
-                Recent Games
-              </h2>
-            </Link>
-            <GameHistory className="w-full" maxEntries={3} />
-          </div>
-        </section>
-      </main>
-      <Footer />
-    </div>
+    <main className="p-8 flex flex-col items-center">
+      <h1 className="mb-8 text-6xl">Chess 42</h1>
+      <p className="text-4xl">Better than chess.com!</p>
+      <section className="grid lg:grid-cols-2 py-10 gap-10 w-full">
+        <div className="bg-linear-110 from-gray-600/50 to bg-neutral-800 p-2 rounded-xl">
+          <Link href="/stats">
+            <h2 className="text-2xl w-full text-center pb-4 pt-2 hover:text-stone-400">
+              Leaderboard
+            </h2>
+          </Link>
+          <LeaderBoard maxEntries={3} />
+        </div>
+        <div className="bg-linear-230 from-gray-600/50 to-neutral-800 p-4 rounded-xl">
+          <Link href="/history">
+            <h2 className="text-2xl text-center pb-4 hover:text-stone-400">
+              Recent Games
+            </h2>
+          </Link>
+          <GameHistory className="w-full" maxEntries={3} />
+        </div>
+      </section>
+    </main>
   );
 }
