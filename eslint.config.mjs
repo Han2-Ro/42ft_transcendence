62d967ead7e060864df4084e66df69454d64a3de
Han2-Ro <127860003+Han2-Ro@users.noreply.github.com>
2026-03-25 21:07:14 +0100

    dev(lint): enforce naming conventions

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -15,8 +15,63 @@ const eslintConfig = defineConfig([
     "*/next-env.d.ts",
   ]),
   {
+    settings: {
+      react: {
+        version: "999.999.999",
+      },
+    },
     rules: {
       semi: ["error", "always"],
+      "@next/next/no-html-link-for-pages": "off",
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: ["function", "method"],
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "variable",
+          format: ["camelCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: ["typeLike"],
+          format: ["PascalCase"],
+        },
+      ],
+    },
+  },
+  {
+    files: ["nextjs/**/*"],
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+    rules: {
+      "@next/next/no-html-link-for-pages": ["error", "nextjs/src/app"],
+      "@typescript-eslint/naming-convention": [
+        "error",
+        {
+          selector: "function",
+          format: ["PascalCase", "camelCase"],
+        },
+        {
+          selector: "method",
+          format: ["camelCase"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: "variable",
+          format: ["camelCase", "PascalCase", "UPPER_CASE"],
+          leadingUnderscore: "allow",
+        },
+        {
+          selector: ["typeLike"],
+          format: ["PascalCase"],
+        },
+      ],
     },
   },
 ]);
