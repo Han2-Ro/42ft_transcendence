4052c8fdec8ed483a09de5b7ba76fd355ae9f743
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2026-04-18 12:55:48 +0200

    feat: style settings page

--- nextjs/src/app/settings/page.tsx
+++ nextjs/src/app/settings/page.tsx
@@ -10,15 +10,15 @@ export default function Page() {
   }
 
   return (
-    <main>
-      <h1>Settings</h1>
-      <h2>Account</h2>
+    <main className=" max-w-lg mx-auto p-2">
+      <h1 className="text-3xl p-3 text-center">Settings</h1>
+      <h2 className="text-xl px-2">Account</h2>
       <hr />
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-between items-center p-2 w-full">
         <p>Username: {user ? user.username : "None"}</p>
         <Button onClick={() => console.log("abc")}>Change</Button>
       </div>
-      <div className="flex flex-row">
+      <div className="flex flex-row justify-between items-center p-2 w-full">
         <p>Password: ****</p>
         <Button onClick={() => console.log("abc")}>Change</Button>
       </div>
