d0329c1298de7e56cbf055f17dd1e625e590f2c1
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2026-04-18 13:58:58 +0200

    fix: login modal on mobile

--- nextjs/src/componets/sidebar/MainMenu.tsx
+++ nextjs/src/componets/sidebar/MainMenu.tsx
@@ -19,7 +19,6 @@ type Props = {
 
 export default function MainMenu({ onClose }: Props) {
   const [showLogin, setShowLogin] = useState(false);
-  const toggleLogin = () => setShowLogin(!showLogin);
   const { user, refreshUser } = useAuthConetxt();
   const router = useRouter();
   const { actions } = useSidebarActions();
@@ -50,7 +49,7 @@ export default function MainMenu({ onClose }: Props) {
 
   return (
     <>
-      {showLogin && <AuthModal onClose={toggleLogin} />}
+      {showLogin && <AuthModal onClose={() => setShowLogin(false)} />}
       <nav className="h-full w-full py-6 flex flex-col justify-center gap-4 items-start">
         <MenuButton
           href="/"
@@ -75,10 +74,7 @@ export default function MainMenu({ onClose }: Props) {
           />
         ) : (
           <MenuButton
-            onClick={() => {
-              toggleLogin();
-              onClose?.();
-            }}
+            onClick={() => setShowLogin(true)}
             icon={<LoginIcon size={20} />}
             label="Log In"
           />
