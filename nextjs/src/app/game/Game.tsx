5381b25d01d57a963548e5f88114678305a004ad
aprevrha <119614085+aprevrha@users.noreply.github.com>
2026-04-21 11:51:33 +0200

    fix: rm game in progress text and paying as

--- nextjs/src/app/game/Game.tsx
+++ nextjs/src/app/game/Game.tsx
@@ -21,9 +21,6 @@ export default function Game({
 }: GameProps) {
   return (
     <div className="text-center flex flex-col justify-center">
-      <div className="mb-4">
-        <h2 className="text-2xl font-semibold">Game in progress</h2>
-      </div>
       {gameType === "chess" || gameType === "timedChess" ? (
         <TwoPlayerBoard
           boardState={boardState}
@@ -40,7 +37,6 @@ export default function Game({
           times={times}
         />
       )}
-      <p className="mt-4">You are playing as {color}</p>
     </div>
   );
 }
