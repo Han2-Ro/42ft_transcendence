4e6e91cb60a8584af4fbfc6d1374534e61bc8597
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2026-04-23 20:27:35 +0200

    basic design

--- nextjs/src/app/game/ConnectFourBoard.tsx
+++ nextjs/src/app/game/ConnectFourBoard.tsx
@@ -18,5 +18,21 @@ export default function ConnectFourBoard({
     onPlayerMove(square - 1);
   };
 
-  return <></>;
+  return (
+    <div className="grid grid-cols-7 p-3 bg-blue-900 rounded-xl">
+      {boardState.board.map((square, index) => (
+        <button
+          key={index}
+          className="size-16"
+          onClick={() => {
+            onPlayerMove(index % 7);
+          }}
+        >
+          <div
+            className={`size-4/5 m-auto rounded-full ${square === "empty" ? "bg-background-primary" : square === "yellow" ? "bg-yellow-400" : "bg-red-600"}`}
+          ></div>
+        </button>
+      ))}
+    </div>
+  );
 }
