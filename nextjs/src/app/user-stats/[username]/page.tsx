59a36e28ebd1aca027e57231dd3551fd964e8a73
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2026-04-24 21:12:12 +0200

    feat: 404 for user not found

--- nextjs/src/app/user-stats/[username]/page.tsx
+++ nextjs/src/app/user-stats/[username]/page.tsx
@@ -2,6 +2,7 @@ import { getUserStats } from "@/lib/auth/actions";
 import StatCards from "./StatCards";
 import ErrorMessage from "@/componets/ErrorMessage";
 import { getWinRatio } from "@/lib/getWinRation";
+import { notFound } from "next/navigation";
 
 export default async function Page({
   params,
@@ -10,42 +11,37 @@ export default async function Page({
 }) {
   const { username } = await params;
   const stats = await getUserStats(username);
+  if ("error" in stats) notFound();
   return (
     <main className="px-4 py-8 size-fit mx-auto">
       <h1 className="text-4xl pb-4 font-bold">
         Stats for <span className="text-accent-primary">{username}</span>
       </h1>
-      {"error" in stats ? (
-        <ErrorMessage errorMsg={stats.error} />
-      ) : (
-        <>
-          <h2 className="text-xl font-bold pb-2">Chess:</h2>
-          <StatCards
-            stats={[
-              { label: "Wins", value: stats.userLookup.wins },
-              { label: "Losses", value: stats.userLookup.losses },
-              { label: "Draws", value: stats.userLookup.draws },
-              { label: "Win Ration", value: getWinRatio(stats.userLookup) },
-            ]}
-          />
-          <h2 className="pb-2 pt-8 font-bold text-xl">Connect 4:</h2>
-          <StatCards
-            stats={[
-              { label: "Wins", value: stats.userLookup.connectWins },
-              { label: "Losses", value: stats.userLookup.connectLosses },
-              { label: "Draws", value: stats.userLookup.connectDraws },
-              {
-                label: "Win Ratio",
-                value: getWinRatio({
-                  wins: stats.userLookup.connectWins,
-                  losses: stats.userLookup.connectLosses,
-                  draws: stats.userLookup.connectDraws,
-                }),
-              },
-            ]}
-          />
-        </>
-      )}
+      <h2 className="text-xl font-bold pb-2">Chess:</h2>
+      <StatCards
+        stats={[
+          { label: "Wins", value: stats.userLookup.wins },
+          { label: "Losses", value: stats.userLookup.losses },
+          { label: "Draws", value: stats.userLookup.draws },
+          { label: "Win Ration", value: getWinRatio(stats.userLookup) },
+        ]}
+      />
+      <h2 className="pb-2 pt-8 font-bold text-xl">Connect 4:</h2>
+      <StatCards
+        stats={[
+          { label: "Wins", value: stats.userLookup.connectWins },
+          { label: "Losses", value: stats.userLookup.connectLosses },
+          { label: "Draws", value: stats.userLookup.connectDraws },
+          {
+            label: "Win Ratio",
+            value: getWinRatio({
+              wins: stats.userLookup.connectWins,
+              losses: stats.userLookup.connectLosses,
+              draws: stats.userLookup.connectDraws,
+            }),
+          },
+        ]}
+      />
     </main>
   );
 }
