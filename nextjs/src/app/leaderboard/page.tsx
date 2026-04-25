91f0f7c9674a1db832c401baaa492f5c84ca1d62
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2026-04-25 20:43:51 +0200

    feat: link between stats, leaderboard, history pages

--- nextjs/src/app/leaderboard/page.tsx
+++ nextjs/src/app/leaderboard/page.tsx
@@ -1,13 +1,30 @@
+import { PlainLink } from "@/componets/PlainLink";
 import LeaderBoard from "./LeaderBoard";
 import { getLeaderboard } from "@/lib/auth/actions";
+import Link from "next/link";
 
 export default async function Page() {
   const entries = await getLeaderboard();
 
   return (
-    <main className="p-4 flex flex-col items-center">
-      <h1 className="pt-4 text-center pb-8 text-3xl font-bold">Leaderboard</h1>
-      <LeaderBoard entries={entries} />
-    </main>
+    <>
+      <div className="bg-background-secondary/50 flex pt-2 flex-row justify-center gap-2">
+        <div className="p-2">
+          <PlainLink href="/user-stats" label="Personal Stats" />
+        </div>
+        <div className="py-2 px-8 bg-background-primary rounded-t-md">
+          <PlainLink href="/leaderboard" label="Leaderboard" />
+        </div>
+        <div className="p-2">
+          <PlainLink href="/history" label="Match History" />
+        </div>
+      </div>
+      <main className="p-4 flex flex-col items-center">
+        <h1 className="pt-4 text-center pb-8 text-3xl font-bold">
+          Leaderboard
+        </h1>
+        <LeaderBoard entries={entries} />
+      </main>
+    </>
   );
 }
