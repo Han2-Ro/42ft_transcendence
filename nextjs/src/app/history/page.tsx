7d5a1a4d48c960718af7db5c0cafdbceb6b88930
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2026-04-26 13:55:25 +0200

    fix: fixes #165

--- nextjs/src/app/history/page.tsx
+++ nextjs/src/app/history/page.tsx
@@ -16,11 +16,11 @@ export default async function Page() {
       <StatsNavigationTabs />
       <main className="flex flex-col items-center">
         <h1 className="p-8 text-3xl font-bold">Your Game History</h1>
-        <h2 className="p-4 text-xl font-bold">Two Player Chess</h2>
+        <h2 className="p-4 text-xl font-bold">Chess: Two Players</h2>
         <GameHistory className="w-full max-w-200" data={history2p} />
-        <h2 className="p-4 mt-4 text-xl font-bold">Four Player Chess</h2>
+        <h2 className="p-4 mt-4 text-xl font-bold">Chess: Four Players</h2>
         <GameHistory className="w-full max-w-200" data={history4p} />
-        <h2 className="p-4 mt-4 text-xl font-bold">Four Player Chess</h2>
+        <h2 className="p-4 mt-4 text-xl font-bold">Connect Four</h2>
         <GameHistory className="w-full max-w-200" data={historyConnect4} />
       </main>
     </>
