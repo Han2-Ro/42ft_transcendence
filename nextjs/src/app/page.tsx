0a4a519c589b61140fe165ef0ba153b39a8cd183
Hannes <127860003+Han2-Ro@users.noreply.github.com>
2026-04-27 10:48:14 +0200

    feat: style frontpage

--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -3,6 +3,9 @@ import Link from "next/link";
 import GameHistory from "./history/GameHistory";
 import { getGameTwoHistory, getLeaderboard } from "@/lib/auth/actions";
 import ErrorMessage from "@/components/ErrorMessage";
+import { Anton } from "next/font/google";
+
+const anton = Anton({ subsets: ["latin"], weight: "400" });
 
 export default async function Home({
   searchParams,
@@ -14,14 +17,21 @@ export default async function Home({
   const { error } = await searchParams;
   return (
     <main className="p-2 md:p-8 flex flex-col items-center">
-      <h1 className="md:mb-8 text-6xl">Chess 42</h1>
       {error === "42_already_linked" && (
         <ErrorMessage errorMsg="This 42 account is already linked to another 42Chess account."></ErrorMessage>
       )}
       {error === "42_no_acc_linked" && (
         <ErrorMessage errorMsg="No 42Chess account is linked to this 42 account."></ErrorMessage>
       )}
-      <p className="text-4xl">Better than chess.com!</p>
+      <h1
+        className={`${anton.className} md:mb-8 text-7xl flex flex-col items-center`}
+        style={{ fontFamily: "Impact, Anton, sans-serif" }}
+      >
+        <div className="text-accent-primary text-[10rem]">42</div>
+        <div>Chess</div>
+      </h1>
+      <p className="text-3xl italic text-gray-400">Better than chess.com!</p>
+      <hr className="w-full my-8 border-t border-gray-300" />
       <section className="lg:grid lg:grid-cols-2 py-10 gap-10 w-full">
         <div className="bg-linear-110 from-gray-600/50 to bg-neutral-800 p-2 mb-8 lg:mb-0 rounded-xl">
           <Link href="/leaderboard">
